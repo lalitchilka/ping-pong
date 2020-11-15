74c30c2d5d38357a3b79e3600e5feebb64d5f67c
lalitchilka <lalitchilka@gmail.com>
2020-11-15 20:08:59 +0530

    feat: create 2 paddles
    Closes: #2

--- pong.py
+++ pong.py
@@ -1,12 +1,28 @@
 import turtle
 
-# create game window
-wn = turtle._Screen()
-wn.title("Pong by @lalitchilka")
+wn = turtle.Screen()
+wn.title("Pong")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5,stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5,stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
 
 # Main game loop
 while True:
